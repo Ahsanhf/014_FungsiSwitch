1d10f641430a5ed55bdbcb6a165e8153ed627a58
ahsanhf <ahsanhf@gmail.com>
2024-03-26 18:23:45 +0700

    Menjalankan pilihan

--- 014_FungsiSwitch.cpp
+++ 014_FungsiSwitch.cpp
@@ -12,24 +12,24 @@ void input()
 	cin >> bilB;
 }
 
-int penjumlahan(Int a, int b)
+int penjumlahan(int a, int b)
 {
 	return a + b;
 }
 
-int pengurangan(Int a, int b)
+int pengurangan(int a, int b)
 {
 	return a - b;
 }
 
-int pembagian(Int a, int b)
+int pembagian(int a, int b)
 {
 	return a / b;
 }
 
-int perkalian(Int a, int b)
+int perkalian(int a, int b)
 {
-	return a % b;
+	return a * b;
 }
 
 int main()
@@ -44,11 +44,27 @@ int main()
 		cout << "3. Pembagian" << endl;
 		cout << "4. Perkalian" << endl;
 		cout << "5. Keluar" << endl;
-		cout << "=====================" endl;
+		cout << "=====================" << endl;
 		cout << "Masukkan menu pilihan : ";
 		cin >> pilihan;
 		
 		switch (pilihan)
-		
+		{
+		case 1:
+			cout << "Hasil Penjumlahan = " << penjumlahan(bilA, bilB) << endl;
+			break;
+		case 2:
+			cout << "Hasil Pengurangan = " << penjumlahan(bilA, bilB) << endl;
+			break;
+		case 3:
+			cout << "Hasil Pembagian = " << penjumlahan(bilA, bilB) << endl;
+			break;
+		case 4:
+			cout << "Hasil Perkalian = " << penjumlahan(bilA, bilB) << endl;
+			break;
+		case 5:
+			break;
+	
+		}
 	}
 }
