c2e856ca061554c9dc08ea4f11fd251fab7cc1fd
ahsanhf <ahsanhf@gmail.com>
2024-03-26 17:45:12 +0700

    Memasukkan pilihan

--- 014_FungsiSwitch.cpp
+++ 014_FungsiSwitch.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int bilA, bilB;
 
+
 void input()
 {
 	cout << "Masukkan bilangan pertama = ";
@@ -31,5 +32,23 @@ int perkalian(Int a, int b)
 	return a % b;
 }
 
-
-
+int main()
+{
+	int pilihan;
+	do
+	{
+		cout << "Kalkulator Sederhana" << endl;
+		cout << "====================" << endl;
+		cout << "1. Penjumlahana" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Pembagian" << endl;
+		cout << "4. Perkalian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "=====================" endl;
+		cout << "Masukkan menu pilihan : ";
+		cin >> pilihan;
+		
+		switch (pilihan)
+		
+	}
+}
